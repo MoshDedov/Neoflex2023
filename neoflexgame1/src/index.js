c22635581c0238ca2eeaaf21f6dc9885ace12421
DiscoDed <damnthisisso@hotmail.com>
2023-03-23 17:54:40 +0400

    Update2

--- neoflexgame1/src/index.js
+++ neoflexgame1/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import GameOne from './GameOne';
+import Timer from './Timer';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GameOne/>
+    <Timer/>
   </React.StrictMode>
 );
 
